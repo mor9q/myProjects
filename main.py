18460709d9907fabe365faa0924faf34dd26709c
mor9q <radmir06x@mail.ru>
2023-11-23 21:02:24 +0500

    312

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from PyQt5.QtGui import QPainter, QBrush, QPen, QColor, QPixmap
 from PyQt5.QtCore import Qt
 from random import randint
 
-class MyWidget(QDialog):
+class MyWidget(QDialog, Ui_Dialog):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.diameter = 0
+        self.color = Qt.black
         self.create_yellow_circle_button.clicked.connect(self.drawCircle)
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setPen(Qt.black)
-        painter.setBrush(Qt.yellow)
+        painter.setPen(self.color)
+        painter.setBrush(self.color)
         painter.drawEllipse(170, 50, self.diameter, self.diameter)
 
     def drawCircle(self):
